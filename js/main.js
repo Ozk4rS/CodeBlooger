5def8922ac27df96fba0d48e3245a5f4e778b4a6
Ozk4rS <88616065+Ozk4rS@users.noreply.github.com>
2021-11-27 20:39:18 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navMenu = document.getElementById('nav-menu'),
+    toogleMenu = document.getElementById('toggle-menu'),
+    closeMenu = document.getElementById('close-menu')
+
+toogleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+})
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
+
+
